4e58d26c30acbe2851f46f53819961049c2d2acc
Viper <80052842+ViperTheGirl@users.noreply.github.com>
2022-02-15 20:20:21 -0500

    Dad

---
+++ fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version 'cerulean'
+
+game {'gta5'}
+
+description 'XO1-loadingScreen'
+version '1.0.0'
+author 'Nss'
+
+files {
+    'song.mp3',
+    'UI.html',
+    'Styling/UI.css'
+}
+
+loadscreen {
+    'UI.html',
+}
+
+loadscreen_cursor 'yes'
